e13e8eaaf4adbe8f56b7ef4c976e043cdbf7f29c
Jannis Krumland <janniskrumland@MacBook-Air-von-Jannis.local>
2026-02-22 19:49:40 +0100

    turn subtessera into tessera_builder class

--- build_pcm_cavity.py
+++ build_pcm_cavity.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from .subtessera import subtessera
+from .tessera_builder import TesseraBuilder
 from .constants import *
 from .sphere import Sphere
 from .tessera import Tessera
@@ -9,12 +9,14 @@ from .connectivity import Connectivity
 from .get_vertex_positions import get_vertex_positions
 
 
-def build_pcm_cavity(subtesserae_per_tessera, minimum_tessera_distance_angstrom, spheres):
+def build_pcm_cavity(subtesserae_per_tessera, minimum_tessera_distance_angstrom, spheres, 
+                     minimum_tessera_area_angstrom2=1e-8):
     for sphere in spheres:
         sphere.convert_to_angstrom()
     vertex_positions = get_vertex_positions()
     connectivity = Connectivity(subtesserae_per_tessera)
     cavity = Cavity()
+    tessera_builder = TesseraBuilder(spheres)
     for isphere, sphere in enumerate(spheres):
         for itess in range(TESSERAE_PER_SPHERE):
             for isubtess in range(subtesserae_per_tessera):
@@ -22,9 +24,9 @@ def build_pcm_cavity(subtesserae_per_tessera, minimum_tessera_distance_angstrom,
                 pts[:, 0] = vertex_positions[connectivity.n0(itess, isubtess), [0, 2, 1]] * sphere.r + sphere.xyz
                 pts[:, 1] = vertex_positions[connectivity.n1(itess, isubtess), [0, 2, 1]] * sphere.r + sphere.xyz
                 pts[:, 2] = vertex_positions[connectivity.n2(itess, isubtess), [0, 2, 1]] * sphere.r + sphere.xyz
-                point, normal, area = subtessera(isphere, spheres, pts)
-                if abs(area) >= M_EPSILON:
-                    cavity.append(Tessera(point, normal, area, sphere.r))
+                tessera = tessera_builder.build(isphere, pts)
+                if tessera.area >= minimum_tessera_area_angstrom2:
+                    cavity.append(tessera)
     cavity.merge_close_tesserae(minimum_tessera_distance_angstrom)
     cavity.convert_to_bohr()
     return cavity
