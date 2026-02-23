f432f6a0aa19b8a263236f6882956fd35b1f2e3e
Jannis Krumland <janniskrumland@MacBook-Air-von-Jannis.local>
2026-02-23 10:50:30 +0100

    replace get_vertex_positions by VertexPositions class

--- build_pcm_cavity.py
+++ build_pcm_cavity.py
@@ -1,30 +1,25 @@
 import numpy as np
 
 from .tessera_builder import TesseraBuilder
-from .constants import *
+from .constants import TESSERAE_PER_SPHERE
 from .sphere import Sphere
 from .tessera import Tessera
 from .cavity import Cavity
-from .connectivity import Connectivity
-from .get_vertex_positions import get_vertex_positions
+from .vertex_positions import VertexPositions
 
 
 def build_pcm_cavity(subtesserae_per_tessera, minimum_tessera_distance_angstrom, spheres, 
                      minimum_tessera_area_angstrom2=1e-8):
     for sphere in spheres:
         sphere.convert_to_angstrom()
-    vertex_positions = get_vertex_positions()
-    connectivity = Connectivity(subtesserae_per_tessera)
+    vertex_positions = VertexPositions(subtesserae_per_tessera)
     cavity = Cavity()
     tessera_builder = TesseraBuilder(spheres)
     for isphere, sphere in enumerate(spheres):
         for itess in range(TESSERAE_PER_SPHERE):
             for isubtess in range(subtesserae_per_tessera):
-                pts = np.zeros((PCM_DIM_SPACE, DIM_TEN), dtype=np.float64) # (1:PCM_DIM_SPACE, 1:DIM_TEN)
-                pts[:, 0] = vertex_positions[connectivity.n0(itess, isubtess), [0, 2, 1]] * sphere.r + sphere.xyz
-                pts[:, 1] = vertex_positions[connectivity.n1(itess, isubtess), [0, 2, 1]] * sphere.r + sphere.xyz
-                pts[:, 2] = vertex_positions[connectivity.n2(itess, isubtess), [0, 2, 1]] * sphere.r + sphere.xyz
-                tessera = tessera_builder.build(isphere, pts)
+                vertices = vertex_positions(sphere, itess, isubtess)
+                tessera = tessera_builder.build(isphere, vertices)
                 if tessera.area >= minimum_tessera_area_angstrom2:
                     cavity.append(tessera)
     cavity.merge_close_tesserae(minimum_tessera_distance_angstrom)
---
+++ vertex_positions.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+from .connectivity import Connectivity
+from .constants import PCM_DIM_SPACE, DIM_TEN
+
+
+POLAR_ANGLES = ( 
+  0.6523581398,  1.107148718, 1.382085796, 
+   1.759506858,  2.034443936, 2.489234514,
+  0.3261790699, 0.5535743589, 
+  0.8559571251, 0.8559571251, 1.017221968,
+   1.229116717,  1.229116717, 1.433327788,
+   1.570796327,  1.570796327, 1.708264866,
+   1.912475937,  1.912475937, 2.124370686,
+   2.285635528,  2.285635528, 2.588018295,
+   2.815413584
+)
+AZIMUTHAL_ANGLES = (
+  0.6283185307, 0.0000000000,
+  0.6283185307, 0.0000000000, 0.6283185307, 
+  0.0000000000, 0.6283185307, 0.0000000000, 
+  0.2520539002, 1.0045831610, 0.6283185307, 
+  0.3293628477, 0.9272742138, 0.0000000000, 
+  0.3141592654, 0.9424777961, 0.6283185307, 
+  0.2989556830, 0.9576813784, 0.0000000000, 
+  0.3762646305, 0.8803724309, 0.6283188307, 
+  0.0000000000
+)
+AZIMUTHAL_STEPS = 5
+AZIMUTHAL_INCREMENT = 2*np.pi / AZIMUTHAL_STEPS
+NORTH_POLE = 0, 0, 1
+SOUTH_POLE = 0, 0, -1
+
+
+class VertexPositions:
+    def __init__(self, subtesserae_per_tessera):
+        self._vertex_positions = self._get_vertex_positions()
+        self._connectivity = Connectivity(subtesserae_per_tessera)
+
+    def __call__(self, sphere, itess, isubtess):
+        pts = np.zeros((PCM_DIM_SPACE, DIM_TEN), dtype=np.float64) # (1:PCM_DIM_SPACE, 1:DIM_TEN)
+        pts[:, 0] = self._vertex_positions[self._connectivity.n0(itess, isubtess), [0, 2, 1]] * sphere.r + sphere.xyz
+        pts[:, 1] = self._vertex_positions[self._connectivity.n1(itess, isubtess), [0, 2, 1]] * sphere.r + sphere.xyz
+        pts[:, 2] = self._vertex_positions[self._connectivity.n2(itess, isubtess), [0, 2, 1]] * sphere.r + sphere.xyz
+        return pts
+
+    def _get_vertex_positions(self):
+        vertex_positions = [NORTH_POLE]
+        for theta, phi_0 in zip(POLAR_ANGLES, AZIMUTHAL_ANGLES):
+            for iphi in range(AZIMUTHAL_STEPS):
+                phi = phi_0 + iphi * AZIMUTHAL_INCREMENT
+                vertex_positions.append(
+                    (np.sin(theta) * np.cos(phi), np.sin(theta) * np.sin(phi), np.cos(theta))
+                )
+        vertex_positions.append(SOUTH_POLE)
+        return np.array(vertex_positions, dtype=np.float64)
+
+
